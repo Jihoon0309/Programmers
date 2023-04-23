67b422d84299d65311e1c95f27cac1e2a66ca396
Jihoon0309 <130656475+Jihoon0309@users.noreply.github.com>
2023-04-23 20:43:50 +0900

    Create 햄버거_만들기.py
    연습 문제

---
+++ Level_1/햄버거_만들기.py
@@ -0,0 +1,11 @@
+def solution(ingredient):
+    count=0
+    hamburger=[]
+
+    for i in ingredient:
+        hamburger.append(i)
+        if hamburger[-4:]==[1,2,3,1]:
+            count+=1
+            del hamburger[-4:]          
+
+    return count
