3beceda520c6b9fa82593c5f310960c28743d8d7
Jihoon0309 <130656475+Jihoon0309@users.noreply.github.com>
2023-05-06 17:13:34 +0900

    Create H_Index.py

---
+++ Level_2/H_Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    citations.sort()
+    count=0
+    
+    for i in range(len(citations)):
+        if citations[i]>=len(citations)-i:
+            count+=1
+    return count
